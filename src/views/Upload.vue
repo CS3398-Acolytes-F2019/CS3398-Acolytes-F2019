093557a330071e6eb4e6d104bbb5f769ca01e5c3
Damen <damen@damenhannah.com>
2019-09-24 15:35:39 -0500

    Refactor boilerplate and create Upload.vue.

---
+++ src/views/Upload.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="upload">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'home',
+  components: {
+  },
+});
+</script>
