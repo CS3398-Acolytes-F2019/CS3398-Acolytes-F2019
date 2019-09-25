b975d07f63a115ecaa2026f05ffc290b84ead7ee
DamenH <damen@damenhannah.com>
2019-09-25 09:36:53 -0500

    Create ProgressCirlce.vue.

---
+++ src/components/ProgressCircle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="progress-circle">
+        Progress Circle WIP
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "progress-circle"
+});
+</script>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="upload">
-    <upload-button></upload-button>
-  </div>
+    <div class="upload">
+        <upload-button></upload-button>
+        <progress-circle></progress-circle>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import UploadButton from "../components/UploadButton.vue";
+import ProgressCircle from "../components/ProgressCircle.vue";
 
 export default Vue.extend({
-  name: "upload",
-  components: { UploadButton }
+    name: "upload",
+    components: {
+        UploadButton,
+        ProgressCircle
+    }
 });
 </script>
