7b519251908e1cbda2eefb85d2cece3c0e22e32f
DamenH <damen@damenhannah.com>
2019-09-27 10:24:04 -0500

    Examples and documentation of how to use storage and database.

--- functions/src/FileController.ts
+++ functions/src/FileController.ts
@@ -1,11 +1,9 @@
 import {Request, Response} from "express";
-import * as admin from 'firebase-admin';
+import { Firebase } from "./Firebase";
 
 export class FileController
 {
     
-
-
     /**
      * Stores the into Firebase Storage and creates a
      * record of it's storage in the database.
@@ -14,10 +12,39 @@ export class FileController
      */
     static store(request: Request, response: Response)
     {
-        const database = admin.firestore();
-        // make sure it's not too big.
 
-        // response.send
+        // something is going to have to be done with it being a streamed upload
+
+        // make sure it's not too big. 1MB for now.
+        
+        // store it in firebase storage
+        // https://firebase.google.com/docs/storage/web/create-reference
+
+        let storage = Firebase.instance.storage();
+
+        // get a reference to storage
+
+        // let storageRef = storage.ref();
+
+        // https://firebase.google.com/docs/storage/web/upload-files
+
+
+
+        
+
+        // create a new fileId by creating a random string and making sure that string hasn't been used.
+        let fileId = "";
+
+        
+
+        // create a record of it, and it's location with 
+        // https://firebase.google.com/docs/database/web/read-and-write
+        let database = Firebase.instance.database();
+        database.ref("files/" + fileId).set({
+            storageUrl: "fakeURL",
+            options: {}
+        });
+
     }
 
     /**
